187347abe5736bec40f9023ab2455074cffa4f7b
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-09-22 17:50:05 -0300

    Create class, import libs, and init function

--- 01 - Create Blockchain/blockchain.py
+++ 01 - Create Blockchain/blockchain.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 22 17:15:37 2023
+import datetime #Every block will have your exacly creation date
+import hashlib #Every block will have your hashs
+import json #Make json data and read on json
 
-@author: r.rocker
-"""
+from flask import Flask, jsonify #jsonify(make post requests and receive responses)
 
+# Part 01, create a Blockchain
+
+class Blockchain: #Class creation
+    def __init__(self): #Init function, self(class variable refer to own class)
+        self.chain = [] #Inicialize array on python
+        self.create_block(proof = 1, previous_has='0') #Method 'create_block'(genesis block) with two parameters
