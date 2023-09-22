2120aa333698ba3d531424159729579bc8cd9288
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-09-22 18:07:50 -0300

    Create_block fuction created

--- 01 - Create Blockchain/blockchain.py
+++ 01 - Create Blockchain/blockchain.py
@@ -9,4 +9,14 @@ from flask import Flask, jsonify #jsonify(make post requests and receive respons
 class Blockchain: #Class creation
     def __init__(self): #Init function, self(class variable refer to own class)
         self.chain = [] #Inicialize array on python
-        self.create_block(proof = 1, previous_has='0') #Method 'create_block'(genesis block) with two parameters
+        self.create_block(proof = 1, previous_hash='0') #Method 'create_block'(genesis block) with two parameters
+        
+    def create_block(self, proof, previous_hash): #Creation 'create_block' method with 3 params, 'self', 'proof', 'previous_hash'
+        block = { #Create dictionary with 4 keys
+            'index': len(self.chain) + 1, #Index, number of block
+            'timestamp': str(datetime.datetime.now()), #Exacly date creation
+            'proof': proof, #Proofwork puzle = nonse
+            'previous_hash': previous_hash #Link with previous_hash
+            }
+        self.chain.append(block) #Add this element to Chain array
+        return block
